4310470cc3d762f5c2790e03c8f6c61709b2ed77
ErmolaevSD <weekendarti@yandex.ru>
2025-08-05 23:35:45 +0300

    Реализация ТЗ

--- src/main/java/ru/practicum/shareit/exception/CreateModelException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class CreateModelException extends RuntimeException {
-    public CreateModelException(String errorMessage) {
-        super(errorMessage);
-    }
-}
