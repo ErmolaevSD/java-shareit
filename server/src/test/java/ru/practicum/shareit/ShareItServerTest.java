60dc0ecd62ed8033ba796f622fcc215454e423a6
ErmolaevSD <weekendarti@yandex.ru>
2025-08-06 01:43:23 +0300

    Реализация ТЗ

---
+++ server/src/test/java/ru/practicum/shareit/ShareItServerTest.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ShareItServerTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void main() {
+        ShareItServer.main(new String[] {});
+    }
+}
